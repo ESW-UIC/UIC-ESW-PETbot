92db025e10dfeedf9b6978eeb642378843d1e8d8
Siddarth Menon <siddarthmenonk@gmail.com>
2025-10-23 19:00:19 -0500

    Updated the Y_MAX_POS to a super high level

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -263,7 +263,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 // Travel limits after homing
 #define X_MAX_POS 180
 #define X_MIN_POS 0
-#define Y_MAX_POS 205
+#define Y_MAX_POS 8000
 #define Y_MIN_POS 0
 #define Z_MAX_POS 185
 #define Z_MIN_POS 0
